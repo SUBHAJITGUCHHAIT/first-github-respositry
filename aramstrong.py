e7ad8be8a19d6723a36604e653f1741dcd28f099
Subhajit Guchhait <168919678+SUBHAJITGUCHHAIT@users.noreply.github.com>
2024-05-16 12:03:43 +0530

    Update aramstrong.py

--- aramstrong.py
+++ aramstrong.py
@@ -1,1 +1,14 @@
+num1=int(input("Enter a number "))
+num=num1
+digits=len(str(num1))
+arm=0
+while num1>0:
+    arm=arm+((num1%10)**digits)
+    num1=num1//10
+if num==arm:
+    print("It is Armstrong Number")
+else:
+    print("It is not Armstrong Number")
+
+
 
